34cb0b56e528434b3fb9dfea58bee5ab58fd60b6
Shin Tanimoto <cel@pos.to>
2016-07-25 05:37:12 +0900

    Run Sleuth / Zipkin via Stream (RabbitMQ)

--- zipkin-server/src/main/java/ninja/cero/ecommerce/zipkin/ZipkinApplication.java
+++ zipkin-server/src/main/java/ninja/cero/ecommerce/zipkin/ZipkinApplication.java
@@ -1,25 +1,13 @@
 package ninja.cero.ecommerce.zipkin;
 
-import org.elasticsearch.Version;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import zipkin.server.EnableZipkinServer;
-import zipkin.storage.StorageComponent;
-import zipkin.storage.elasticsearch.ElasticsearchStorage;
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
 
 @SpringBootApplication
-@EnableZipkinServer
+@EnableZipkinStreamServer
 public class ZipkinApplication {
 	public static void main(String[] args) {
-		System.out.println(Version.CURRENT);
 		SpringApplication.run(ZipkinApplication.class, args);
 	}
-
-	@Bean
-	StorageComponent storage() {
-		return ElasticsearchStorage.builder().build();
-	}
-
 }
